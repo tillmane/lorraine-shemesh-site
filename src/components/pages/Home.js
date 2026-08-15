ac2774ab2f3032d0f1b246675218d07e2ad7e035
Tillman (Zack) Erb <tillmane@gmail.com>
2026-08-15 16:33:04 -0700

    new home page image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return( 
         <div className="image-container">
-            {<img className="individual-image-main" src={'https://lorraineshemeshsiteimgs2.s3.us-east-2.amazonaws.com/studio_Lorraine_Cutouts.jpg'} alt="Lorraine Shemesh in the studio"/>}
+            {<img className="individual-image-main" src={'https://lorraineshemeshsiteimgs2.s3.us-east-2.amazonaws.com/studio_Lorraine_Cutouts_blue.jpg'} alt="Lorraine Shemesh in the studio"/>}
         </div>
     )
 }
